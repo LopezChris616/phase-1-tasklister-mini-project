945730562b5455692eadefbe723070360a4e3362
Christian Lopez <lopezchristian1610@gmail.com>
2023-03-30 22:52:50 -0400

    tasklister main objectives completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  submitForm();
 });
+
+function submitForm() {
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    createTodoElement(input);
+  });
+}
+
+function createTodoElement(input) {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  const todoList = document.getElementById("tasks");
+
+  li.textContent = `${input.value} `;
+  button.textContent = "X";
+  li.appendChild(button);
+  todoList.appendChild(li);
+
+  deleteTodo(button);
+}
+
+function deleteTodo(button) {
+  button.addEventListener("click", () => {
+    button.parentNode.remove();
+  })
+}
